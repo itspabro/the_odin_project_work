fa7d047e88eca5663b271e53f5d076dd91b10bbd
Pablo Ramirez <pabloqramirez@gmail.com>
2025-02-26 19:33:21 -0800

    Complete javascript-exercises 08
    Create functions that perform math calculations.
    Increased difficulty of tests to enhance enjoyment of the exercise.

--- javascript-exercises/08_calculator/calculator.js
+++ javascript-exercises/08_calculator/calculator.js
@@ -1,25 +1,65 @@
-const add = function() {
-	
+const add = function(first, second) {
+  return first + second
 };
 
-const subtract = function() {
-	
+const subtract = function(first, second) {
+  return first - second
 };
 
-const sum = function() {
-	
+const sum = function(...num) {
+  console.log(num)
+  let newNum
+  let output = 0
+  let shouldFail = false
+
+  newNum = num.flat(Infinity)
+
+  console.log(newNum)
+
+  newNum.forEach( (value) => {
+    if ( !Number.isInteger(value) ) {
+      shouldFail = true;
+      return;
+    }
+
+    output = output + Number(value)
+  })
+
+  console.log(output)
+
+  if ( shouldFail ) {
+    return "ERROR"
+  }
+
+  return output
 };
 
-const multiply = function() {
+const multiply = function(nums) {
+  let output = 1
 
+  nums.forEach( (value) => {
+    output = output * value
+  });
+
+  return output
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  return base ** exponent
 };
 
-const factorial = function() {
-	
+const factorial = function(integer) {
+  let output = integer
+
+  if ( integer === 0 ) {
+    output = 1
+  }
+
+  for ( i = integer; i > 1; i-- ) {
+    output = output * ( i - 1 )
+  }
+
+  return output
 };
 
 // Do not edit below this line
